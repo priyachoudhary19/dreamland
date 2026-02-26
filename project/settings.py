65e2f04ba69ae2f884ab604a14072b0f4ec37ee1
Priya Choudhary <priyachoudhary19@gmail.com>
2026-02-26 17:05:24 +0530

    Fixed ALLOWED_HOSTS for deployment

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ie-w!_v@5bpz%i$nuda7!i47392!%u@c7_c9%&m)te17*7#*t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dreamland-ulmc.onrender.com',
+    'localhost',
+    '127.0.0.1',]
 
 
 # Application definition
@@ -114,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 LOGIN_URL = 'login/'
