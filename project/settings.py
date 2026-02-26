247221d5566fc6de3fd8f839be9e283564567b6f
Priya Choudhary <priyachoudhary19@gmail.com>
2026-02-26 17:25:47 +0530

    static configured properly

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,8 +121,10 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'accounts' / 'static',
 ]
